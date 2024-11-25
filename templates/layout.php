9ace338a4d0481aa2e206a99e7641ca64f92ba06
ClementRollin <clemrollin01@gmail.com>
2024-11-25 12:25:28 +0100

    Change css

--- templates/layout.php
+++ templates/layout.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         html, body {
-            height: 100%;
+            height: 100vh;
         }
         header {
             padding: 1rem 0;
