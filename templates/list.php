e0fc95e01240c3ff29fbffe6d5578c6a94fa9c49
ClementRollin <clemrollin01@gmail.com>
2024-11-25 14:31:56 +0100

    Add number of articles

--- templates/list.php
+++ templates/list.php
@@ -33,6 +33,7 @@
     }
 </style>
 <h2 class="mb-4">Tous les articles</h2>
+<p>Nombres d'articles : <?= getTotalPostCount() ?></p>
 <div class="row g-4">
     <?php foreach ($posts as $post): ?>
         <div class="col-md-4">
